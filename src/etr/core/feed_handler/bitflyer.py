2ce3a52366b3f15abcfe43914026171ce8f67fea
a4q6 <takumi.arai2@gmail.com>
2025-05-01 23:06:00 +0900

    remove await

--- src/etr/core/feed_handler/bitflyer.py
+++ src/etr/core/feed_handler/bitflyer.py
@@ -82,7 +82,6 @@ class BitFlyerSocketClient:
                 subscribe_message = {"method": "subscribe", "params": {"channel": channel}}
                 await websocket.send(json.dumps(subscribe_message))
                 self.logger.info(f"Send request for '{channel}'")
-            await asyncio.sleep(1)
 
             try:
                 asyncio.create_task(self.heartbeat(websocket))
