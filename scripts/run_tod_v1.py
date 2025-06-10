764c3a67ecf00f3c6b65686a7fb5e638358127c0
a4q6 <takumi.arai2@gmail.com>
2025-06-10 23:57:40 +0900

    add TOD entry point script

---
+++ scripts/run_tod_v1.py
@@ -0,0 +1,63 @@
+import asyncio
+import datetime
+import pandas as pd
+from etr.core.ws import LocalWsClient
+from etr.strategy.tod.tod_v1 import TOD_v1
+from etr.core.api_client import CoincheckRestClient
+
+
+if __name__ == "__main__":
+
+    # global COUNTER
+    # COUNTER = 0
+    # async def my_print(msg):
+    #     print(msg)
+    #     global COUNTER
+    #     COUNTER += 1
+    #     if COUNTER > 10:
+    #         raise RuntimeError()
+
+    # initialize
+    client = CoincheckRestClient(log_name="tod_v1.log")
+    strategy = TOD_v1(
+        venue="coincheck",
+        entry_config=[
+            {"sym": "XRPJPY", "start": datetime.time(1, 30), "holding_minutes": 60 * 4, "sl_level": 300, "side": 1, "amount": 3, "spread_filter": 50},
+            {"sym": "XRPJPY", "start": datetime.time(7, 0), "holding_minutes": 60 * 5 + 30, "sl_level": 300, "side": 1, "amount": 3, "spread_filter": 50},
+            {"sym": "XRPJPY", "start": datetime.time(16, 30), "holding_minutes": 60 * 4, "sl_level": 300, "side": 1, "amount": 3, "spread_filter": 50},
+        ],
+        client=client, 
+        log_file="tod_v1.log")
+    strategy.warmup()
+    client.register_strategy(strategy)
+    subscriber = LocalWsClient(callbacks=[strategy.on_message], log_file="tod_v1.log")
+    # subscriber = LocalWsClient(callbacks=[my_print], log_file="tod_v1.log")
+
+    # ws subsciber loop
+    async def start_subscribe():
+        await subscriber.connect()
+        await asyncio.sleep(1)
+        await subscriber.subscribe([
+            {"_data_type": "Rate", "venue": "coincheck", "sym": "XRPJPY"},
+            {"_data_type": "MarketTrade", "venue": "coincheck", "sym": "XRPJPY"},
+        ])
+        now = pd.Timestamp.today()
+        stop = (now + pd.Timedelta("1d")).ceil("1d")
+        await asyncio.sleep((stop - now).total_seconds())
+
+    # start main logic
+    async def main():
+        try:
+            await asyncio.gather(
+                start_subscribe(),
+                client.loop_fetch_transactions(),
+            )
+        except KeyboardInterrupt:
+            print("Interrupted by user")
+        finally:
+            print("Closing connections...")
+            await asyncio.gather(
+                subscriber.close(),
+            )
+
+    asyncio.run(main())
---
+++ src/etr/core/api_client/__init__.py
@@ -0,0 +1,1 @@
+from .coincheck import CoincheckRestClient
